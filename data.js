01c46c49b24984064a85579814892f09b473f0ea
Harsh Sri <harshsri5310@gmail.com>
2026-03-18 15:41:43 +0530

    add compiled server files

---
+++ data.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.vocabularyTerms = exports.scenarios = exports.lessons = exports.grammarTopics = void 0;
+const mock_data_1 = require("../lib/mock-data");
+Object.defineProperty(exports, "grammarTopics", { enumerable: true, get: function () { return mock_data_1.grammarTopics; } });
+Object.defineProperty(exports, "lessons", { enumerable: true, get: function () { return mock_data_1.lessons; } });
+Object.defineProperty(exports, "scenarios", { enumerable: true, get: function () { return mock_data_1.scenarios; } });
+Object.defineProperty(exports, "vocabularyTerms", { enumerable: true, get: function () { return mock_data_1.vocabularyTerms; } });
